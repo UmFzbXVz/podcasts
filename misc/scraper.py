ce706d9d7c625663093a4dab82d2cf6d43fe40f8
UmFzbXVz <63111902+UmFzbXVz@users.noreply.github.com>
2023-05-03 15:24:12 +0200

    Update scraper.py

--- misc/scraper.py
+++ misc/scraper.py
@@ -5,6 +5,7 @@ with open('fil_med_json', 'r') as f:
     podcasts = json.load(f)
 
 for podcast_id, podcast_data in podcasts.items():
+    podcast_data.setdefault('episodes', [])
     page_number = 0
     while True:
         response = requests.get(f"https://app.listentonews.com/api/v4/user/channels/{podcast_id}/items?order_by=newest&filter=&page={page_number}&per=60&auth={auth_key}")
@@ -12,16 +13,16 @@ for podcast_id, podcast_data in podcasts.items():
             break
         episodes = response.json()
         for episode in episodes:
-            podcast_data.setdefault('episodes', []).append({
+            podcast_data['episodes'].append({
                 'id': episode['id'],
                 'uri': episode['uri'],
                 'title': episode['title'],
                 'duration': episode['duration']['formatted'],
                 'publish_time': episode['publish_time'],
                 'description': episode['host_and_speaking_info'],
-                # tilføj andet der skal hentes
+                # Tilføj andet du vil scrape fra hjemmesiden via API-kaldet
             })
         page_number += 1
 
 with open('fil_med_json', 'w') as f:
-    json.dump(podcasts, f, indent=2)
+    json.dump(podcasts, f, indent=4)
