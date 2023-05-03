ef4f777b7f8acb8d54c4e004c6c7c7988e405808
UmFzbXVz <63111902+UmFzbXVz@users.noreply.github.com>
2023-05-03 15:12:10 +0200

    Første udkast...
    Script til at løbe alle kendte podcasts og deres afsnit igennem for id, audio_url, title, duration, pubDate og description. 
    
        "4008": {
            "alt_text": "Den Korte Radioavis",
            "image_url": "https://assets.listentonews.com/uploads/provider/logo/412/Den_korte_radioavis.png",
            "url": "https://web.24syv.dk/channels/4008",
            "description": "Den Korte Radioavis giver dig s\u00e5 korte og skarpe nyheder som overhovedet muligt, altsammen under sloganet \u201cklar, skarp, parat!\u201d. Programmet bev\u00e6rtes af seniorkorrespondent Kirsten Birgit Schi\u00f8tz Kretz H\u00f8rsholm, og med sig i studiet har hun nyhedsredakt\u00f8ren Rasmus Bruun.\r\n\r\nMed deadline hvert femtende minut vil de sammen arbejde m\u00e5lrettet, knaldhurtigt og kompromisl\u00f8st p\u00e5 at g\u00f8re nyhederne kortere og kortere, og skarpere og skarpere."
    
    -> 
        "4008": {
            "alt_text": "Den Korte Radioavis",
            "image_url": "https://assets.listentonews.com/uploads/provider/logo/412/Den_korte_radioavis.png",
            "url": "https://web.24syv.dk/channels/4008",
            "description": "Den Korte Radioavis giver dig s\u00e5 korte og skarpe nyheder som overhovedet muligt, altsammen under sloganet \u201cklar, skarp, parat!\u201d. Programmet bev\u00e6rtes af seniorkorrespondent Kirsten Birgit Schi\u00f8tz Kretz H\u00f8rsholm, og med sig i studiet har hun nyhedsredakt\u00f8ren Rasmus Bruun.\r\n\r\nMed deadline hvert femtende minut vil de sammen arbejde m\u00e5lrettet, knaldhurtigt og kompromisl\u00f8st p\u00e5 at g\u00f8re nyhederne kortere og kortere, og skarpere og skarpere.",
            "episodes": [
                {
                    "id": 107591,
                    "uri": "https://assets.listentonews.com/uploads/audio_clip/rss_file/107591/57238450-audio.mp3",
                    "title": "Det sidste ord",
                    "duration": "00:02:43:000",
                    "publish_time": "2019-10-31T11:05:00Z",
                    "description": "<p><p>I knapt halvanden m\u00e5ned har vi talt om et v\u00e6ld af dagsaktuelle emner. Det har v\u00e6ret vildt fedt, lidt skr\u00e6mmende, men allermest meget meningsfyldt. Madam Bl\u00e5 er simpelthen imponeret over niveauet af debatten og imponeret over hvor reflekterede og sjove lyttere hun har haft. Hun har gr\u00e6dt og grinet bag mikrofonen og tusind tak for det. I dag samler vi op. Hvad fik du ikke sagt? I dag f\u00e5r lytterne det sidste ord. Tak for nu fra Madam Bl\u00e5.</p>\r\n</p><p><a href=\"http://arkiv.radio24syv.dk/photo/28382036/det-sidste-ord\"><img src=\"http://arkiv.radio24syv.dk/27288171/28382036/d452ac218083c19e95ae4f90aaf87838/standard/download-thumbnail.jpg\" width=\"1400\" height=\"1400\"/></a></p> " }, ...

---
+++ misc/scraper.py
@@ -0,0 +1,27 @@
+import json
+import requests
+
+with open('fil_med_json', 'r') as f:
+    podcasts = json.load(f)
+
+for podcast_id, podcast_data in podcasts.items():
+    page_number = 0
+    while True:
+        response = requests.get(f"https://app.listentonews.com/api/v4/user/channels/{podcast_id}/items?order_by=newest&filter=&page={page_number}&per=60&auth={auth_key}")
+        if not response.json():
+            break
+        episodes = response.json()
+        for episode in episodes:
+            podcast_data.setdefault('episodes', []).append({
+                'id': episode['id'],
+                'uri': episode['uri'],
+                'title': episode['title'],
+                'duration': episode['duration']['formatted'],
+                'publish_time': episode['publish_time'],
+                'description': episode['host_and_speaking_info'],
+                # tilføj andet der skal hentes
+            })
+        page_number += 1
+
+with open('fil_med_json', 'w') as f:
+    json.dump(podcasts, f, indent=2)
