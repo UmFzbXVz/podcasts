24d593755560d7196f2e9cabecb4e02962c4237f
UmFzbXVz <63111902+UmFzbXVz@users.noreply.github.com>
2023-05-04 14:18:26 +0200

    Korrigeret pubDate. Refaktoreret kode.

--- misc/weaver.py
+++ misc/weaver.py
@@ -5,95 +5,85 @@ import xml.dom.minidom
 from xml.sax.saxutils import escape
 from bs4 import BeautifulSoup
 
-with open('fil_med_json', 'r') as f:
-    podcasts = json.load(f)
+def create_rss_feed(podcasts):
+    for podcast_id, podcast_data in podcasts.items():
+        rss = ET.Element('rss', {'version': '2.0'})
+        channel = ET.SubElement(rss, 'channel')
 
-# Iterer gennem hver podcast i JSON-filen
-for podcast_id, podcast_data in podcasts.items():
+        append_basic_tags(channel, podcast_data)
+        append_itunes_tags(channel, podcast_data)
+        
+        seen_ids = set()
+        for episode in podcast_data['episodes']:
+            if episode['id'] not in seen_ids:
+                seen_ids.add(episode['id'])
+                item = ET.SubElement(channel, 'item')
+                append_episode_basic_tags(item, episode)
+                append_episode_itunes_tags(item, episode)
 
-    # Opret et RSS-element med version 2.0
-    rss = ET.Element('rss', {'version': '2.0'})
+        write_rss_feed_to_file(rss, podcast_data)
 
-    # Opret et kanal-element og tilføj det til RSS-elementet
-    channel = ET.SubElement(rss, 'channel')
-
-    # Tilføj titel, link, beskrivelse og sprog til kanal-elementet
+def append_basic_tags(channel, podcast_data):
     ET.SubElement(channel, 'title').text = escape(podcast_data['alt_text'])
     ET.SubElement(channel, 'link').text = podcast_data['url']
     ET.SubElement(channel, 'description').text = escape(podcast_data['description'])
     ET.SubElement(channel, 'language').text = 'da-DK'
 
-    # Tilføj iTunes-specifikke tags til kanal-elementet
+def append_itunes_tags(channel, podcast_data):
     itunes_ns = 'http://www.itunes.com/dtds/podcast-1.0.dtd'
     ET.register_namespace('itunes', itunes_ns)
-    if 'subtitle' in podcast_data:
-        ET.SubElement(channel, '{%s}subtitle' % itunes_ns).text = escape(podcast_data['subtitle'])
-    if 'summary' in podcast_data:
-        ET.SubElement(channel, '{%s}summary' % itunes_ns).text = escape(podcast_data['summary'])
-    if 'author' in podcast_data:
-        ET.SubElement(channel, '{%s}author' % itunes_ns).text = escape(podcast_data['author'])
+
+    for tag in ['subtitle', 'summary', 'author']:
+        if tag in podcast_data:
+            ET.SubElement(channel, '{%s}%s' % (itunes_ns, tag)).text = escape(podcast_data[tag])
+
     if 'image_url' in podcast_data:
         image_url = podcast_data['image_url']
         ET.SubElement(channel, '{%s}image' % itunes_ns, {'href': image_url})
     if 'category' in podcast_data:
         ET.SubElement(channel, '{%s}category' % itunes_ns, {'text': escape(podcast_data['category'])})
 
-    # Hold styr på set af set episode-ID'er
-    seen_ids = set()
+def append_episode_basic_tags(item, episode):
+    publish_time = episode['publish_time'].replace('Z', '+0000')
+    ET.SubElement(item, 'title').text = escape(episode['title'])
+    ET.SubElement(item, 'link').text = episode['uri']
+    description_text = get_description_text(episode)
+    ET.SubElement(item, 'description').text = description_text
+    ET.SubElement(item, 'guid', {'isPermaLink': 'true'}).text = episode['uri']
+    pub_date = datetime.strptime(publish_time, '%Y-%m-%dT%H:%M:%S%z').strftime('%a, %d %b %Y %H:%M:%S %z')
+    ET.SubElement(item, 'pubDate').text = pub_date
 
-    # Iterer gennem hver episode for denne podcast
-    for episode in podcast_data['episodes']:
+def get_description_text(episode):
+    description_html = episode.get('description', '')
+    if description_html is not None and description_html.strip() != '':
+        description_text = BeautifulSoup(description_html, 'html.parser').get_text()
+    else:
+        description_text = ''
+    return description_text
 
-        # Spring over duplikerede episoder
-        if episode['id'] in seen_ids:
-            continue
-        seen_ids.add(episode['id'])
+def append_episode_itunes_tags(item, episode):
+    itunes_ns = 'http://www.itunes.com/dtds/podcast-1.0.dtd'
+    duration_formatted = convert_duration_to_formatted_string(episode['duration'])
+    ET.SubElement(item, '{%s}duration' % itunes_ns).text = duration_formatted
 
-        # Opret et item-element og tilføj det til kanal-elementet
-        publish_time = episode['publish_time'].replace('Z', '+0000')
-        item = ET.SubElement(channel, 'item')
-        ET.SubElement(item, 'title').text = escape(episode['title'])
-        ET.SubElement(item, 'link').text = episode['uri']
-        
-        # Fix for handling empty and NoneType descriptions
-        description_html = episode.get('description', '')
-        if description_html is not None and description_html.strip() != '':
-            description_text = BeautifulSoup(description_html, 'html.parser').get_text()
-        else:
-            description_text = ''
-        
-        ET.SubElement(item, 'description').text = description_text
-        
-        ET.SubElement(item, 'guid', {'isPermaLink': 'true'}).text = episode['uri']
-        pub_date = datetime.strptime(publish_time, '%Y-%m-%dT%H:%M:%S%z').strftime('%a, %d %b %Y %H:%M:%S %z')
-        ET.SubElement(item, 'pubDate').text = pub_date
+    for tag in ['subtitle', 'summary', 'author']:
+        if tag in episode:
+            ET.SubElement(item, '{%s}%s' % (itunes_ns, tag)).text = escape(episode[tag])
 
-        # Beregn længden af lydfilen i sekunder
-        duration = episode['duration']
-        duration_parts = list(map(int, duration.split(':')))
-        if len(duration_parts) == 2:
-            duration_parts.insert(0, 0)
-        length = duration_parts[0] * 3600 + duration_parts[1] * 60 + duration_parts[2]
+def convert_duration_to_formatted_string(duration):
+    duration_parts = list(map(int, duration.split(':')))
+    if len(duration_parts) == 2:
+        duration_parts.insert(0, 0)
+    duration_formatted = f"{duration_parts[0]:02d}:{duration_parts[1]:02d}:{duration_parts[2]:02d}"
+    return duration_formatted
 
-        # Sæt "enclosure"-tagget med URL'en til lydfilen, længden i sekunder og MIME-typen
-        ET.SubElement(item, 'enclosure', {
-            'url': episode['uri'],
-            'length': str(length),
-            'type': 'audio/mpeg'
-        })
-
-        # Tilføj iTunes-specifikke tags til hver episode
-        duration_formatted = f"{duration_parts[0]:02d}:{duration_parts[1]:02d}:{duration_parts[2]:02d}"
-        ET.SubElement(item, '{%s}duration' % itunes_ns).text = duration_formatted
-        if 'subtitle' in episode:
-            ET.SubElement(item, '{%s}subtitle' % itunes_ns).text = escape(episode['subtitle'])
-        if 'summary' in episode:
-            ET.SubElement(item, '{%s}summary' % itunes_ns).text = escape(episode['summary'])
-        if 'author' in episode:
-            ET.SubElement(item, '{%s}author' % itunes_ns).text = escape(episode['author'])
-
-    # Gem RSS-feedet i en fil
+def write_rss_feed_to_file(rss, podcast_data):
     file_name = podcast_data['alt_text'].replace('/', '_').replace('?', '_').replace(' ', '_').lower()
     with open(f"{file_name}.rss", 'w', encoding='utf-8') as feed_file:
         xml_string = xml.dom.minidom.parseString(ET.tostring(rss)).toprettyxml()
         feed_file.write(xml_string)
+
+with open('data.json', 'r') as f:
+    podcasts = json.load(f)
+
+create_rss_feed(podcasts)
